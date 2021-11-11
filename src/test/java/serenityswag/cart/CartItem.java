9a9883b3dec8f9a1b10a6d88714634a88d065e61
willokans <Slide2014!>
2021-11-11 15:58:52 +0000

    work in progress

--- src/test/java/serenityswag/cart/CartItem.java
+++ src/test/java/serenityswag/cart/CartItem.java
@@ -1,4 +1,51 @@
 package serenityswag.cart;
 
-public record CartItem (String title, String description, Double price)  {
+import java.util.Objects;
+
+public final class CartItem {
+    private final String title;
+    private final String description;
+    private final Double price;
+
+    public CartItem(String title, String description, Double price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public Double price() {
+        return price;
+    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj == this) return true;
+//        if (obj == null || obj.getClass() != this.getClass()) return false;
+//        var that = (CartItem) obj;
+//        return Objects.equals(this.title, that.title) &&
+//                Objects.equals(this.description, that.description) &&
+//                Objects.equals(this.price, that.price);
+//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, price);
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem[" +
+                "title=" + title + ", " +
+                "description=" + description + ", " +
+                "price=" + price + ']';
+    }
+
 }
